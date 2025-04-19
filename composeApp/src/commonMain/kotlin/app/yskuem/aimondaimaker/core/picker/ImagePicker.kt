325a666e6dd274b6701ab7c12e8753a415347331
yskuem <xrayu0022@gmail.com>
2025-04-19 18:46:04 +0900

    fix: lint

--- composeApp/src/commonMain/kotlin/app/yskuem/aimondaimaker/core/picker/ImagePicker.kt
+++ composeApp/src/commonMain/kotlin/app/yskuem/aimondaimaker/core/picker/ImagePicker.kt
@@ -1,8 +1,6 @@
 package app.yskuem.aimondaimaker.core.picker
 
-import io.github.vinceglb.filekit.FileKit
 import io.github.vinceglb.filekit.PlatformFile
-import io.github.vinceglb.filekit.dialogs.openFilePicker
 
 interface ImagePicker {
     suspend fun pickImage(): PlatformFile?
