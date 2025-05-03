f019afe235e6d8ffac5ea41188a65476909bb8a1
yskuem <xrayu0022@gmail.com>
2025-05-03 12:00:49 +0900

    fix: import

--- composeApp/src/commonMain/kotlin/app/yskuem/aimondaimaker/data/api/response/ProjectResponse.kt
+++ composeApp/src/commonMain/kotlin/app/yskuem/aimondaimaker/data/api/response/ProjectResponse.kt
@@ -1,10 +1,11 @@
 package app.yskuem.aimondaimaker.data.api.response
 
 import app.yskuem.aimondaimaker.data.supabase.SupabaseColumnName
+import app.yskuem.aimondaimaker.data.supabase.SupabaseColumnName.Project.CREATED_AT
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.SerialName
 @Serializable
 data class ProjectResponse(
     val id: String,
-    @SerialName(SupabaseColumnName.Project.CREATED_AT) val createdAt: String
+    @SerialName(CREATED_AT) val createdAt: String
 )
