46cdf115a924f4fdb978e3e1656e1dfd321d4ee1
yskuem <xrayu0022@gmail.com>
2025-05-07 22:56:06 +0900

    fix: delete unused import

--- composeApp/src/commonMain/kotlin/app/yskuem/aimondaimaker/core/di/InitKoin.kt
+++ composeApp/src/commonMain/kotlin/app/yskuem/aimondaimaker/core/di/InitKoin.kt
@@ -6,8 +6,6 @@ import app.yskuem.aimondaimaker.data.repository.di.repositoryModule
 import app.yskuem.aimondaimaker.feature.di.viewModelModule
 import app.yskuem.aimondaimaker.feature.select_alubum_or_camera.di.selectAlbumOrCameraModule
 import org.koin.core.KoinApplication
-import org.koin.core.context.startKoin
-import org.koin.dsl.KoinAppDeclaration
 
 
 fun initKoin() {
