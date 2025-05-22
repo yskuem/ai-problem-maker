5596a4d147c6e9e81b4fbb7728fad4d2cc6d493f
yskuem <xrayu0022@gmail.com>
2025-05-22 23:01:31 +0900

    feat: back button

--- composeApp/src/commonMain/kotlin/app/yskuem/aimondaimaker/feature/note/ui/NoteAppScreen.kt
+++ composeApp/src/commonMain/kotlin/app/yskuem/aimondaimaker/feature/note/ui/NoteAppScreen.kt
@@ -2,28 +2,75 @@ package app.yskuem.aimondaimaker.feature.note.ui
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import app.yskuem.aimondaimaker.domain.entity.Note
 import com.multiplatform.webview.web.WebView
 import com.multiplatform.webview.web.rememberWebViewStateWithHTMLData
 
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.ui.graphics.Color
+
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NoteApp(
     note: Note,
     onBack: () -> Unit,
 ) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        val htmlState = rememberWebViewStateWithHTMLData(note.html)
-        WebView(
-            state = htmlState,
-            modifier = Modifier.fillMaxSize()
-        )
+    // Scaffold provides an app bar and content area
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(note.title)
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = onBack
+                    ) {
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "戻る")
+                    }
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = Color.White,
+                    titleContentColor = MaterialTheme.colorScheme.onSurface,
+                    navigationIconContentColor = MaterialTheme.colorScheme.onSurface
+                )
+            )
+        },
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            contentAlignment = Alignment.Center
+        ) {
+            val htmlState = rememberWebViewStateWithHTMLData(note.html)
+            WebView(
+                state = htmlState,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
     }
 }
+
+
