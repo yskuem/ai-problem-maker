ddc2aa4c1543a719571e84f505b439c9617435eb
yskuem <xrayu0022@gmail.com>
2025-05-27 16:45:15 +0900

    feat: アップデート判断ロジックの作成

--- composeApp/src/commonMain/kotlin/app/yskuem/aimondaimaker/usecase/CheckUpdateUseCaseImpl.kt
+++ composeApp/src/commonMain/kotlin/app/yskuem/aimondaimaker/usecase/CheckUpdateUseCaseImpl.kt
@@ -1,12 +1,24 @@
 package app.yskuem.aimondaimaker.usecase
 
 import app.yskuem.aimondaimaker.domain.data.repository.VersionRepository
+import app.yskuem.aimondaimaker.domain.status.CheckUpdateStatus
 import app.yskuem.aimondaimaker.domain.usecase.CheckUpdateUseCase
 
 class CheckUpdateUseCaseImpl(
     private val versionRepository: VersionRepository,
 ): CheckUpdateUseCase {
-    init {
-
+    override suspend fun checkUpdate(): CheckUpdateStatus {
+        versionRepository.fetchAndActivate()
+        val latestVersion = versionRepository.fetchLastestAppVersion()
+        val forcedUpdateMinVersion = versionRepository.fetchForcedUpdateMinVersion()
+        val currentVersion = versionRepository.getCurrentAppVersion()
+        if(currentVersion <= forcedUpdateMinVersion) {
+            return CheckUpdateStatus.UPDATED_NEEDED
+        }
+        if(currentVersion < latestVersion) {
+            return CheckUpdateStatus.HAVE_LATEST_APP_VERSION
+        }
+        return CheckUpdateStatus.NONE
     }
+
 }
