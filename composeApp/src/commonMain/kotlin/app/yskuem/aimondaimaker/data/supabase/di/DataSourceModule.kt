67d0772ca56f191887543ba85541ea7883fbbf36
yskuem <xrayu0022@gmail.com>
2025-06-08 09:13:52 +0900

    fix: env settings

--- composeApp/src/commonMain/kotlin/app/yskuem/aimondaimaker/data/supabase/di/DataSourceModule.kt
+++ composeApp/src/commonMain/kotlin/app/yskuem/aimondaimaker/data/supabase/di/DataSourceModule.kt
@@ -10,8 +10,8 @@ import org.koin.dsl.module
 val supabaseModule = module {
     single {
         createSupabaseClient(
-            supabaseUrl = "https://ozpxhlodwuwifuzbnryy.supabase.co",
-            supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96cHhobG9kd3V3aWZ1emJucnl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg3NDY5NTgsImV4cCI6MjA2NDMyMjk1OH0.M05unoLVOCoAz0a4aV-gwUCHaEKPQNJfQYFNUg7oGaQ",
+            supabaseUrl = "https://yexqdytmyenkzpornjhx.supabase.co",//"https://ozpxhlodwuwifuzbnryy.supabase.co",
+            supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlleHFkeXRteWVua3pwb3Juamh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYyNzY0NDgsImV4cCI6MjA2MTg1MjQ0OH0.MXdfk2WqDCOMmsw9zerJ4KpCicm2Yyl8LhqirKCXZzU"//"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96cHhobG9kd3V3aWZ1emJucnl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg3NDY5NTgsImV4cCI6MjA2NDMyMjk1OH0.M05unoLVOCoAz0a4aV-gwUCHaEKPQNJfQYFNUg7oGaQ",
         ) {
             install(Auth)
             install(Postgrest)
