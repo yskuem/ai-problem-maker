08c5572be44ec2c019780ce3a5d5b4c6c3953d41
yskuem <xrayu0022@gmail.com>
2025-06-21 11:26:52 +0900

    update: (build.gradle.kts) increment build number

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -115,7 +115,7 @@ android {
         applicationId = "app.yskuem.aimondaimaker"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.0"
     }
     packaging {
