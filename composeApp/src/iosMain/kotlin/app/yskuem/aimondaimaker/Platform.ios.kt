7b38630a508f5cd01dab1c472264bd78a62bc458
yskuem <xrayu0022@gmail.com>
2025-08-07 22:06:21 +0900

    fix: syntax error

--- composeApp/src/iosMain/kotlin/app/yskuem/aimondaimaker/Platform.ios.kt
+++ composeApp/src/iosMain/kotlin/app/yskuem/aimondaimaker/Platform.ios.kt
@@ -1,11 +1,11 @@
 package app.yskuem.aimondaimaker
 
 import platform.UIKit.UIDevice
-import platform.UIKit.UIUserInterfaceIdiom
+import platform.UIKit.UIUserInterfaceIdiomPad
 
 class IOSPlatform : Platform {
     override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
-    override val isIPad: Boolean = UIDevice.currentDevice.userInterfaceIdiom == UIUserInterfaceIdiom.UIUserInterfaceIdiomPad
+    override val isIPad: Boolean = UIDevice.currentDevice.userInterfaceIdiom == UIUserInterfaceIdiomPad
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()
