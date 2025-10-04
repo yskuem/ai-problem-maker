d00ee3c0dda234d02937f9eee5c1545de133cd7b
yskuem <xrayu0022@gmail.com>
2025-10-04 11:16:55 +0900

    fix: delete unused code

--- composeApp/src/iosMain/kotlin/app/yskuem/aimondaimaker/core/util/ContextFactory.kt
+++
@@ -1,9 +0,0 @@
-package app.yskuem.aimondaimaker.core.util
-
-import platform.Foundation.NSBundle
-import platform.UIKit.UIApplication
-
-actual class ContextFactory {
-    // RootViewController can be used to identify your current screen
-    actual fun getActivity(): Any = UIApplication.sharedApplication.keyWindow?.rootViewController ?: ""
-}
