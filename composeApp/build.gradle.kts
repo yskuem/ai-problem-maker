fb74e0702266d23e482bce6b430521fe78b9decf
yskuem <xrayu0022@gmail.com>
2025-10-23 23:13:00 +0900

    chore: update build version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -130,8 +130,8 @@ android {
         applicationId = "app.yskuem.aimondaimaker"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 13
-        versionName = "1.0.1"
+        versionCode = 14
+        versionName = "1.0.2"
     }
     packaging {
         resources {
