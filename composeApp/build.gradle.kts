d4800ec9fee1f83dc5d41730031621fe7e0b73fc
yskuem <xrayu0022@gmail.com>
2025-11-05 00:07:47 +0900

    fix: backhandler

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -65,7 +65,7 @@ kotlin {
             implementation(compose.foundation)
             implementation(compose.material)
             implementation(compose.ui)
-            //implementation(libs.ui.backhandler)
+            implementation(libs.ui.backhandler)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
