1996c45877328c0de424192827c839c2349c4929
yskuem <xrayu0022@gmail.com>
2025-11-05 23:11:47 +0900

    fix: remove unused import

--- composeApp/src/commonMain/kotlin/app/yskuem/aimondaimaker/feature/onboarding/AppIntroductionScreen.kt
+++ composeApp/src/commonMain/kotlin/app/yskuem/aimondaimaker/feature/onboarding/AppIntroductionScreen.kt
@@ -1,13 +1,9 @@
 package app.yskuem.aimondaimaker.feature.onboarding
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.input.key.Key.Companion.R
-import app.yskuem.aimondaimaker.core.util.getLocalVideoUrl
 import app.yskuem.aimondaimaker.feature.select_project.ui.SelectProjectScreen
-import app.yskuem.aimondaimaker.feature.update_check.UpdateCheckScreen
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
-import io.github.yskuem.onboarding.api.IntroductionScreen
 
 class AppIntroductionScreen: Screen {
     @Composable
