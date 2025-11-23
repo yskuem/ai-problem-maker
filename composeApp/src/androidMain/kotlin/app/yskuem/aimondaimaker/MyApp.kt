bcba577556bef7b59c3b09ddffdf7fbe48d667b7
yskuem <xrayu0022@gmail.com>
2025-11-23 11:26:03 +0900

    fix: delete unused import

--- composeApp/src/androidMain/kotlin/app/yskuem/aimondaimaker/MyApp.kt
+++ composeApp/src/androidMain/kotlin/app/yskuem/aimondaimaker/MyApp.kt
@@ -3,7 +3,6 @@ package app.yskuem.aimondaimaker
 import android.app.Application
 import android.content.Context
 import app.yskuem.aimondaimaker.core.di.diModules
-import app.yskuem.aimondaimaker.feature.subscription.RevenueCatInitializer
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
